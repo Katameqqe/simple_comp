f9e1d1fcc5b7543de5961a2bc26392eeb0afa2bf
Lumber4ack <protsyukvitalik@gmail.com>
2025-04-03 23:16:32 +0200

    Make it look more good but uniques func dont work as expepcted

--- src/unique.c
+++ src/unique.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define CHUNK 256
+#define BUF_CHUNK 128
 #define CHUNK_SIZE 1024*1024
 
 #define min(x, y) (x < y ? x : y)
@@ -18,13 +18,11 @@ Function to make set of unique bytes from text
 */
 unsigned short int uniques(unsigned char* r_list,FILE* fptr){
     char f_dup = 0x00;
-    //unsigned char* list = (unsigned char*)calloc(256,1);
     unsigned char* rbyte_buffer = (unsigned char* )malloc(CHUNK_SIZE);
     unsigned int max_read;
     unsigned int i = 0;
-    unsigned short int b_i = 0;
+    unsigned char b_i = 0;
 
-    //int res = fread(&buff,1,1,fptr);
     max_read = fread(rbyte_buffer,1,CHUNK_SIZE,fptr);
 
     r_list[0] = rbyte_buffer[0];
@@ -46,23 +44,18 @@ unsigned short int uniques(unsigned char* r_list,FILE* fptr){
         */
         if (f_dup == 0x00){
             r_list[b_i] = rbyte_buffer[i];
-            if (b_i == 255){
-                printf("\n%d\n",b_i);
-                return b_i;
+            if (b_i >= BUF_CHUNK-1){
+                return b_i+1;
             }
             b_i+=1;
-            printf("%d ",b_i);
         }
         i+=1;
         if(i > max_read){
-            //max_read = fread(rbyte_buffer,1,CHUNK_SIZE,fptr);
             max_read = 0;
             i=0;
         }
-        //printf("%s",list);
     }
     free(rbyte_buffer);
-    printf("\n%d \nEND\n",b_i);
     return b_i;
 }
 
@@ -71,30 +64,26 @@ unsigned char uniq(unsigned char* r_list, FILE* fptr){
 }
 
 void compress(FILE* input_file, FILE* output_file){
-    unsigned char* buf = (unsigned char*)calloc(CHUNK,1);
-    unsigned char *found;
-    unsigned char index;
+    unsigned char* uniq_buff = (unsigned char*)calloc(BUF_CHUNK,1);
+    unsigned char *f_index;
+    unsigned char buff_i;
     size_t max_read;
     unsigned char* rbyte_buffer = (unsigned char* )malloc(CHUNK_SIZE);
     unsigned char* wbyte_buffer = (unsigned char* )malloc(CHUNK_SIZE);
-    unsigned int rbyte_ofset = 0;
-    unsigned int wbyte_ofset = 0;
+    unsigned int rbyte_offset = 0;
+    unsigned int wbyte_offset = 0;
 
 
     size_t f_size;
     
-    const unsigned char X = uniques(buf,input_file);
-    //printf("%d\n", X);
-    wbyte_buffer[wbyte_ofset] = X;
-    wbyte_ofset+=1;
-    qsort(buf, X, 1, compare);
-    printf("%x\n",wbyte_buffer);
-    memcpy(wbyte_buffer+wbyte_ofset,buf,X);
-    printf("%x\n",wbyte_buffer);
-    wbyte_ofset+=X;
-    printf("%s\n",buf);
-    //fwrite(&X, 1, 1, output_file);
-    //fwrite(buf, X, 1, output_file);
+    const unsigned char X = uniques(uniq_buff,input_file);
+    if (X>BUF_CHUNK) return -1;
+    
+    wbyte_buffer[wbyte_offset] = X;
+    wbyte_offset+=1;
+    qsort(uniq_buff, X, 1, compare);
+    memcpy(wbyte_buffer+wbyte_offset,uniq_buff,X);
+    wbyte_offset+=X;
 
     const int N = ceil(log2(X));
 
@@ -102,93 +91,80 @@ void compress(FILE* input_file, FILE* output_file){
     f_size = ftell(input_file);
     fseek(input_file, 0L, SEEK_SET);
 
-    unsigned char ofset = 3;
+    unsigned char offset = 3;
 
     const int R = (8 - (3 + f_size * N) % 8) % 8;
     unsigned int need_wb = (((f_size * N)+3+R)/8)+X;
-    unsigned char bytes = R<<(8-ofset) & 0xff;
+    unsigned char byte = R<<(8-offset) & 0xff;
 
-    printf("%d %d %d %d %d\n", X, N, R, f_size, need_wb);
+    printf("X:%d N:%d R:%d\n", X, N, R);
 
     max_read = fread(rbyte_buffer, 1, CHUNK_SIZE,input_file);
-    //printf("%s\nEND\n",rbyte_buffer);
     while (max_read > 0){
-        found = (unsigned char *)bsearch(&rbyte_buffer[rbyte_ofset], buf, X, 1, compare);
-        index = (found-buf) & 0xff;
-        index <<= 8-N;
-        bytes |= (index>>ofset);
-        if (ofset >= 8-N){
-            wbyte_buffer[wbyte_ofset]=bytes;
-            wbyte_ofset+=1;
-            bytes = index << (8-ofset);
-            if (wbyte_ofset >= min(need_wb,CHUNK_SIZE)){
-                fwrite(wbyte_buffer,1,wbyte_ofset,output_file);
-                need_wb -= wbyte_ofset;
-                //printf("%s\nEND OF STRING\n\n",wbyte_buffer);
-                wbyte_ofset = 0;
+        f_index = (unsigned char *)bsearch(&rbyte_buffer[rbyte_offset], uniq_buff, X, 1, compare);
+        buff_i = (f_index-uniq_buff) & 0xff;
+        buff_i <<= 8-N;
+        byte |= (buff_i>>offset);
+        if (offset >= 8-N){
+            wbyte_buffer[wbyte_offset]=byte;
+            wbyte_offset+=1;
+            byte = buff_i << (8-offset);
+            if (wbyte_offset >= min(need_wb,CHUNK_SIZE)){
+                fwrite(wbyte_buffer,1,wbyte_offset,output_file);
+                need_wb -= wbyte_offset;
+                wbyte_offset = 0;
             }
         }
-        rbyte_ofset++;
-        ofset=(ofset+N)%8;
-        if (rbyte_ofset >= min(CHUNK_SIZE,max_read)){
+        rbyte_offset++;
+        offset=(offset+N)%8;
+        if (rbyte_offset >= min(CHUNK_SIZE,max_read)){
             max_read = fread(rbyte_buffer, 1, CHUNK_SIZE,input_file);
-            //printf("%s\nEND\n",rbyte_buffer);
-            rbyte_ofset=0;
+            rbyte_offset=0;
         }
     }
     if (R>0){
-        printf("\n%d %d\n",wbyte_ofset, need_wb+wbyte_ofset);
-        fwrite(&bytes,1,1,output_file);
+        fwrite(&byte,1,1,output_file);
     }
-    printf("1\n");
+    free(uniq_buff);
     free(rbyte_buffer);
-    printf("2\n");
     free(wbyte_buffer);
-    printf("3\n");
 }
 
 void decompress(FILE* input_file, FILE* output_file){
     unsigned char X;
     unsigned char byte;
     size_t max_readed;
-    size_t d_size;
-    unsigned int wi = 0;
+    size_t f_size;
+    unsigned int w_offset = 0;
     unsigned char* rbyte_buffer = (unsigned char* )malloc(CHUNK_SIZE);
     unsigned char* point_buffer = rbyte_buffer;
     unsigned char* wbyte_buffer = (unsigned char* )malloc(CHUNK_SIZE);
     
     fread(&X, 1, 1, input_file);
-    printf("%d \n",X);
     
-    unsigned char* buf = malloc(X);
-    fread(buf, 1, X, input_file);
-    for (unsigned char i = 0; i < X; i++){
-        //printf("%X ",buf[i]);
-    }
-    printf("\n");
-    printf("%d ",X);
-    const int N = ceil(log2(X));
-    printf("%d ",N);
+    if (X>BUF_CHUNK) return -1;
 
-    unsigned char ofset = 3;
+    unsigned char* uniq_buff = malloc(X);
+    fread(uniq_buff, 1, X, input_file);
+
+    const int N = ceil(log2(X));
+
+    unsigned char offset = 3;
     char need_bits = 0;
 
     fread(&byte, 1, 1, input_file);
-    const unsigned char R = byte>>(8-ofset);
+
+    const unsigned char R = byte>>(8-offset);
     fseek(input_file, -1L, SEEK_CUR);
-    printf("%d ",R);
     
     fseek(input_file, 0L, SEEK_END);
-    d_size = ftell(input_file)-(X+1);
+    f_size = ftell(input_file)-(X+1);
     fseek(input_file, (1+X), SEEK_SET);
-    printf("%d ",d_size);
-    const int b_size = (((d_size*8)-(3+R))/N);
-    printf("%d\n",b_size);
+
+    const int b_size = (((f_size*8)-(3+R))/N);
     unsigned int need_size = b_size;
     
-    // Bottom algorythm not working FIX IT
     max_readed = fread(rbyte_buffer,1,CHUNK_SIZE,input_file);
-    //while(fread(&rbyte,1,1,input_file) > 0){
     for(int x = 0; x < b_size; x++){
         if (point_buffer-rbyte_buffer >= CHUNK_SIZE-1){
                 rbyte_buffer[0] = rbyte_buffer[max_readed-1];
@@ -198,49 +174,40 @@ void decompress(FILE* input_file, FILE* output_file){
                 }
                 point_buffer=rbyte_buffer;
             }
-            byte = *point_buffer & (((1<<N)-1)<<(8-N))>>ofset;
-            need_bits = (8-ofset)-N;
+            byte = *point_buffer & (((1<<N)-1)<<(8-N))>>offset;
+            need_bits = (8-offset)-N;
             if (need_bits <= 0){
                 byte = ((byte<<-need_bits) | (*(point_buffer+1) >> (8+need_bits)));
                 point_buffer++;
             }else{
-                byte >>= (8-(N+ofset));
+                byte >>= (8-(N+offset));
             }
-            ofset = (ofset+N)%8;
-                //printf("%d ",byte);
-            wbyte_buffer[wi] = buf[byte];
-            wi++;
-            if(wi>=min(CHUNK_SIZE,need_size)){
-                wbyte_buffer[wi] = buf[byte];
-                printf("%d %x %x %x\n", wi,wbyte_buffer[wi-2],wbyte_buffer[wi-1],wbyte_buffer[wi]);
-                fwrite(wbyte_buffer,1, min(CHUNK_SIZE,wi),output_file);
-                need_size-=wi;
-                wi=0;
-                //printf("%d \n",min(CHUNK_SIZE,b_size));
+            offset = (offset+N)%8;
+            wbyte_buffer[w_offset] = uniq_buff[byte];
+            w_offset++;
+            if(w_offset>=min(CHUNK_SIZE,need_size)){
+                wbyte_buffer[w_offset] = uniq_buff[byte];
+                fwrite(wbyte_buffer,1, min(CHUNK_SIZE,w_offset),output_file);
+                need_size-=w_offset;
+                w_offset=0;
             }
-            //fwrite(wbyte_buffer,1,1,output_file);
     }
-    printf("\n");
     free(wbyte_buffer);
     free(rbyte_buffer);
-    free(buf);
-    //fwrite(wbyte_buffer,1,(1+X+b_size),output_file);
+    free(uniq_buff);
 }
 
-int main(void)
-{
+int main(int argc, char* argv[]){
+    //for (int i = argc; i>0;i--){
+    //    printf("%s\n",argv[i]);
+    //}
+
     FILE* fptr;
     FILE* wptr;
     FILE* zptr;
 
-    fptr = fopen("sample.txt","rb");
+    fptr = fopen("test.txt","rb");
     wptr = fopen("testW.txt","wb");
-
-    //unsigned char* c[1024]; 
-
-    //fread(c,1,1024,fptr);
-
-    //fseek(fptr,-1,SEEK_CUR);
 
     compress(fptr, wptr);
 
@@ -248,7 +215,7 @@ int main(void)
     fclose(wptr);
 
     wptr = fopen("testW.txt","rb");
-    zptr = fopen("sample1.txt","wb");
+    zptr = fopen("testZ.txt","wb");
 
     decompress(wptr, zptr);
 
